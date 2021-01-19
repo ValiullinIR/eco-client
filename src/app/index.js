a4b509d365bccb23e03d1fd6ae1de044ae577cc5
loliallen <maxim.kasakin@yahoo.com>
2021-01-20 01:53:08 +0300

    addded

--- src/app/index.js
+++ src/app/index.js
@@ -1,28 +1,12 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Switch, Route, Redirect, useHistory } from "react-router"
+import React from 'react'
+import { Switch, Route } from "react-router"
 import { FilterPage } from '../pages/FiltersPage'
 import { IndexPage } from '../pages/IndexPage'
 import { MapPage } from '../pages/MapPage'
 import { RecPointsPage } from '../pages/RecPointsPage'
 
 export const App = () => {
-    // const history = useHistory()
-    // const dispatch = useDispatch()
-    // const login_state = useSelector(state => state.app.login_state)
-    // // const alert_state = useSelector(state => state.app.alert)
-
-    // useEffect(() => {
-    //     dispatch(authmeAction())
-    // }, [])
-    // // const handleCloseAlert = () => {
-    // //     dispatch(hideAlert())
-    // // }
-    // useEffect(() => {
-    //     if (login_state === "not authed")
-    //         history.push('/login')
-    // }, [login_state])
-
+    
     return (
         <Switch>
 
@@ -33,20 +17,4 @@ export const App = () => {
             
         </Switch>
     )
-    {/* <Switch>
-        {login_state !== "init" &&
-            <>
-                <Route exact path="/" component={() => <Redirect to="/login" />} />
-                <Route path="/login" component={() => <LoginPage />} />
-                <ProtectedRoute path="/manage" component={() => <AdminPage />} />
-            </>
-        }
-    </Switch>
-    <Snackbar
-        open={alert_state.open}
-        autoHideDuration={3000}
-        onClose={handleCloseAlert}
-    >
-        <Alert onClose={handleCloseAlert} severity={alert_state.type}>{alert_state.message}</Alert>
-    </Snackbar> */}
 }
