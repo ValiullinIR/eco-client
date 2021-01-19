ffdbc13ee9d54dbb6b712e95f8a4a3cab2e67954
loliallen <maxim.kasakin@yahoo.com>
2021-01-20 02:13:30 +0300

    rm redux ext

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,5 @@ export const saga = createSagaMiddleware()
 export const store = createStore(combinedReducers, compose(
     applyMiddleware(
         thunk, saga
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 ))
